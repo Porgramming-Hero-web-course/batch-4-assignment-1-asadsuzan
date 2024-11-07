d607a197e4ef60ec1477f18a7a0fc1dcf565c606
suzan <asadsuzan7@gmail.com>
2024-11-07 14:40:26 +0600

    solved problem number 06

---
+++ src/problem-6.ts
@@ -0,0 +1,11 @@
+// Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
+
+interface Profile {
+    name: string;
+    age: number;
+    email: string
+}
+
+function updateProfile(fullObj: Profile, partialObj: Profile | { name: string } | { age: number } | { email: string }): Profile {
+    return { ...fullObj, ...partialObj }
+}
